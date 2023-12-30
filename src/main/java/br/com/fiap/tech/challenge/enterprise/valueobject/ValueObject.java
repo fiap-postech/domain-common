9c3f5c356b7a5fe7ea9509865db119d9e50380d6
Eberson Oliveira <eberson.olivera@gmail.com>
2023-12-29 22:23:08 -0300

    feat: added equals and haschcode to value object

--- src/main/java/br/com/fiap/tech/challenge/enterprise/valueobject/ValueObject.java
+++ src/main/java/br/com/fiap/tech/challenge/enterprise/valueobject/ValueObject.java
@@ -4,6 +4,9 @@ import br.com.fiap.tech.challenge.AssertionConcern;
 
 import java.io.Serial;
 
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
+
 public abstract class ValueObject extends AssertionConcern {
     @Serial
     private static final long serialVersionUID = -7416337849677971827L;
@@ -11,5 +14,13 @@ public abstract class ValueObject extends AssertionConcern {
     protected ValueObject() {
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        return reflectionEquals(this, obj, true);
+    }
 
+    @Override
+    public int hashCode() {
+        return reflectionHashCode(this, true);
+    }
 }
